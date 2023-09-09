5461d7c4615c43d7fbe913d92fc2874a1c1e4ad2
Kaspar <kaspar.olman97gmail.com>
2023-09-09 09:53:02 +0300

    adding students

--- ContosoUniversity/ContosoUniversity/Data/DbInitializer.cs
+++ ContosoUniversity/ContosoUniversity/Data/DbInitializer.cs
@@ -1,6 +1,27 @@
-﻿namespace ContosoUniversity.Data
+﻿using ContosoUniversity.Models;
+using System;
+using System.Linq;
+
+namespace ContosoUniversity.Data
 {
-    public class DbInitializer
+    public static class DbInitializer
     {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Students.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var students = new Student[]
+            {
+            new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+            new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            }
+
     }
 }
+
