335314e2d5c723cb44929025b770e8ac73081106
Kaspar <kaspar.olman97gmail.com>
2023-09-09 09:10:07 +0300

    Course folder start

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+
+    }
+}
