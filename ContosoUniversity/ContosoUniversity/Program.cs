1ef37a04bc327974187329787e1f1a1a04d485be
Kaspar <kaspar.olman97gmail.com>
2023-09-12 23:34:10 +0300

    End of page one

--- ContosoUniversity/ContosoUniversity/Program.cs
+++ ContosoUniversity/ContosoUniversity/Program.cs
@@ -1,21 +1,78 @@
 using ContosoUniversity.Data;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
+using Microsoft.Extensions.Options;
 
 namespace ContosoUniversity
 {
     public class Program
     {
+
+
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
 
-            CreateDbIfNotExists(host);
 
-            host.Run();
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+            
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            //app.CreateDbIfNotExists();
+            
+
+            //Dispose the context when the Initialize method completes as shown in the following code:
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<SchoolContext>();
+                context.Database.EnsureCreated();
+                DbInitializer.Initialize(context);
+            }
+
+
+            app.Run();
+
         }
 
         private static void CreateDbIfNotExists(IHost host)
@@ -36,11 +93,6 @@ namespace ContosoUniversity
             }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+
     }
 }
