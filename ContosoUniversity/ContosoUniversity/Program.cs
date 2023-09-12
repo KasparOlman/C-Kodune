3d7c8aa3a4deb18f47cf6f2e339e6d74a025243b
Kaspar <kaspar.olman97gmail.com>
2023-09-12 21:26:24 +0300

    .

--- ContosoUniversity/ContosoUniversity/Program.cs
+++ ContosoUniversity/ContosoUniversity/Program.cs
@@ -13,5 +13,25 @@ namespace ContosoUniversity
 
             host.Run();
         }
+
+        private static void CreateDbIfNotExist(IHost host)
+        {
+            using (var scope =host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
+        public static IHostBuilder CreateHostBuiler(string[] args)=>
     }
 }
