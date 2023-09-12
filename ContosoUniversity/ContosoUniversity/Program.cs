0b00980ec62e60200fde44bfbe50fafa23693880
Kaspar <kaspar.olman97gmail.com>
2023-09-12 21:20:01 +0300

    editing Program.cs

--- ContosoUniversity/ContosoUniversity/Program.cs
+++ ContosoUniversity/ContosoUniversity/Program.cs
@@ -1,2 +1,10 @@
 using ContosoUniversity.Data;
 using Microsoft.Extensions.DependencyInjection;
+
+namespace ContosoUniversity
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+        }
+}
