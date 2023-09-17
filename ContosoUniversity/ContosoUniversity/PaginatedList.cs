d14022e0251da0b9bb311acb52ea12e0f13c1162
Kaspar <kaspar.olman97gmail.com>
2023-09-17 14:46:19 +0300

    .

---
+++ ContosoUniversity/ContosoUniversity/PaginatedList.cs
@@ -0,0 +1,14 @@
+﻿namespace ContosoUniversity
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+    }
+}
