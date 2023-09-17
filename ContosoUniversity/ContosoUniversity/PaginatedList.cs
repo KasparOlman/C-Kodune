29eaaf95c375b683e9b18d5490133976f9159eb5
Kaspar <kaspar.olman97gmail.com>
2023-09-17 14:50:43 +0300

    .

--- ContosoUniversity/ContosoUniversity/PaginatedList.cs
+++ ContosoUniversity/ContosoUniversity/PaginatedList.cs
@@ -19,7 +19,8 @@ namespace ContosoUniversity
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source , int pageIndex, int pageSize)
         {
             var count = await source.CountAsync();
-            var
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize). ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
 }
