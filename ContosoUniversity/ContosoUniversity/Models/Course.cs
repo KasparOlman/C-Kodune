b83b2da98ff113f5511d92e1364cf979e7de41d7
Kaspar <kaspar.olman97gmail.com>
2023-09-19 08:40:09 +0300

    .

--- ContosoUniversity/ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -7,11 +7,20 @@ namespace ContosoUniversity.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+
         [Display(Name = "Number")]
 
         public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+        
+        [Range(0, 5)]
+
         public int Credits { get; set; }
+
+        public Department Department { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
