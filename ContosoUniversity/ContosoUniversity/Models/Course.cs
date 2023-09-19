c2b97bbfacf0d86da11318b9cc65c7c12589e5d9
Kaspar <kaspar.olman97gmail.com>
2023-09-19 08:38:46 +0300

    Course fixes

--- ContosoUniversity/ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
@@ -6,6 +7,8 @@ namespace ContosoUniversity.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+
         public int CourseID { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
