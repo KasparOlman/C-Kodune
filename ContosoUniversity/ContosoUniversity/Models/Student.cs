ffa70fde59d42fdeee0be6f571cd06452dbd3609
Kaspar <kaspar.olman97gmail.com>
2023-09-19 08:35:07 +0300

    .

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -11,12 +11,15 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
         [Required]
-
         [StringLength(50)]
+        [Display(Name = "Last Name")]
 
         public string LastName { get; set; }
+        [Required]
         [StringLength(50)]
         [Column("FirstName")]
+        [Display(Name = "First Name")]
+
 
 
         public string FirstMidName { get; set; }
